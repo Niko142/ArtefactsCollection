63aa3cf79db929453f60538b420db41f8e8d144a
Niko142 <baya29.03.1977@gmail.com>
2025-02-08 23:17:49 +0400

    init project, add header and card, add fonts,images

---
+++ src/data/data.js
@@ -0,0 +1,6 @@
+const cardItems = [
+  { id: 1, image: "", title: "", text: "" },
+  { id: 2, image: "", title: "", text: "" },
+  { id: 3, image: "", title: "", text: "" },
+  { id: 4, image: "", title: "", text: "" },
+];
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+const input = document.querySelectorAll("input[type='text']");
+
+// document.querySelector('#app').innerHTML = ``
